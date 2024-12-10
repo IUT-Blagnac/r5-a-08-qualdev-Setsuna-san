57267903d0175d25ea50e717b78eab3503b0b2aa
alexis Christaud-braize <126158656+Setsuna-san@users.noreply.github.com>
2024-12-10 12:34:33 +0100

    TP1

---
+++ demo/src/test/java/com/example/StepDefinitions.java
@@ -0,0 +1,45 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefinitions {
+    @Given("an example scenario")
+    public void anExampleScenario() {
+    }
+
+    @When("all step definitions are implemented")
+    public void allStepDefinitionsAreImplemented() {
+    }
+
+    @Then("the scenario passes")
+    public void theScenarioPasses() {
+    }
+
+    private String today;
+    private String actualAnswer;
+
+    @Given("today is {string}")
+    public void today_is(String day) {
+        today = day;
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_friday_yet() {
+        actualAnswer = IsItFriday.isItFriday(today);
+    }
+
+    @Then("I should be told {string}")
+    public void i_should_be_told(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
+    }
+}
+
+class IsItFriday {
+    static String isItFriday(String today) {
+        return "Friday".equals(today) ? "TGIF" : "Nope";
+    }
+}
